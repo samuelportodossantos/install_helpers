d5a80876def60eb1084994e3b6283238f9cca4d3
Samuel Porto <samuelportodossantos@gmail.com>
2022-08-06 18:52:13 -0300

    feat: a script to install node

---
+++ install_node.sh
@@ -0,0 +1,23 @@
+node_uri=https://nodejs.org/dist/v16.16.0/node-v16.16.0-linux-x64.tar.xz
+node_version=node-v16.16.0-linux-x64
+linux_user=supermoykadev
+node_export_bin='export NODEJS_HOME=/opt/node/bin'
+node_export_export='export PATH=$NODEJS_HOME:$PATH'
+config_path=/home/$linux_user/
+
+wget $node_uri
+tar -xvf $node_version".tar.xz"
+rm -f $node_version".tar.xz"
+mv $node_version node
+sudo chmod -R 755 /opt
+sudo rm -rf /opt/node
+sudo mv  node /opt/
+
+sudo echo $node_export_bin >> $config_path'.profile'
+sudo echo $node_export_export >> $config_path'.profile'
+
+sudo echo $node_export_bin >> $config_path'.bashrc'
+sudo echo $node_export_export >> $config_path'.bashrc'
+
+sudo echo $node_export_bin >> $config_path'.zshrc'
+sudo echo $node_export_export >> $config_path'.zshrc'
